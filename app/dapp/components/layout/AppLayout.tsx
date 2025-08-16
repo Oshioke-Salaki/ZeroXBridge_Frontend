fa3d2d867019470d4e9ffef378cf4b546be43722
Ugonna Paul Dike <ugonnadike001@gmail.com>
2025-08-16 12:51:05 +0100

    Revert "refactor coming soon page to match Figma layout (#161)" (#164)
    This reverts commit d5fcd3a538c5a81f31f76734ca8d97152b32e0e0.

--- app/dapp/components/layout/AppLayout.tsx
+++ app/dapp/components/layout/AppLayout.tsx
@@ -4,23 +4,9 @@ import React, { useState } from "react";
 import Topbar from "./Topbar";
 import Sidebar from "./Sidebar";
 import { ConnectWalletModal } from "../../dashboard/components/connect-wallet";
-import { usePathname } from "next/navigation";
-import { ComingSoonFooter } from "./Footer";
 
-interface AppLayoutProps {
-  children: React.ReactNode;
-  /** the comming soon page has a different layout interms of spacing
-   * the gradient banner touches the edges of it parent, the app layout.
-   * with the current setup/style of the AppLayout, we need to have an optional prop
-   * to help ud sepcifiy whether to use the default padding or not.
-   */
-  layoutPadding?: boolean;
-}
-
-function AppLayout({ children, layoutPadding = true }: AppLayoutProps) {
+function AppLayout({ children }: { children: React.ReactNode }) {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-  const pathname = usePathname();
-  const isComingSoon = pathname === "/dapp/coming-soon"
 
   return (
     <>
@@ -34,12 +20,8 @@ function AppLayout({ children, layoutPadding = true }: AppLayoutProps) {
             isOpen={isSidebarOpen}
             onClose={() => setIsSidebarOpen(false)}
           />
-          <div
-            className={`flex-1 h-full relative overflow-y-auto ${layoutPadding ? "p-[22px] pt-10" : ""}  text-primary-text`}
-          >
+          <div className="flex-1 h-full overflow-y-auto p-[22px] pt-10  text-primary-text">
             {children}
-
-            {isComingSoon && <ComingSoonFooter />}
           </div>
         </div>
       </div>
--- app/dapp/layout.tsx
+++ app/dapp/layout.tsx
@@ -5,22 +5,17 @@ import { StarknetProvider } from "../context/starknet-provider";
 import AppLayout from "./components/layout/AppLayout";
 import { config } from "../config";
 import { Toaster } from "sonner";
-import { usePathname } from "next/navigation";
 
 function layout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const pathname = usePathname();
-  const comingSoon = pathname === "/dapp/coming-soon";
   return (
     <>
       <WagmiProvider config={config}>
         <StarknetProvider>
-          <AppLayout layoutPadding={comingSoon ? false : true}>
-            {children}
-          </AppLayout>
+          <AppLayout>{children}</AppLayout>
         </StarknetProvider>
       </WagmiProvider>
       <Toaster
